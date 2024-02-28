a9a97fd6109a108abd081e9c5bd88c1b3140b91d
Jivane Perich <jivane.perich@gmail.com>
2024-02-28 22:50:47 +0100

    layout articles

--- sg-aa-front/astro.config.mjs
+++ sg-aa-front/astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  prefetch: true,
+  prefetch: false,
   integrations: [tailwind()],
   image: {
     domains: ['127.0.0.1', 'localhost', 'placekitten.com']
