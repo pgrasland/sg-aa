f61e52c121e0bcba2e9b70eabb50c2f3041b6a7c
Jivane Perich <jivane.perich@gmail.com>
2024-03-04 22:29:59 +0100

    dist folder for test

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+const isBuild = import.meta.env.PROD;
+
 // https://astro.build/config
 export default defineConfig({
-  prefetch: false,
+  prefetch: isBuild ? true: false,
   integrations: [tailwind()],
   image: {
     domains: ['127.0.0.1', 'localhost', 'placekitten.com']
