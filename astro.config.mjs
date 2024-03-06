449ac8d86f8a3249f45ad4f6193a3ba23e9ad3b5
Jivane Perich <jivane.perich@gmail.com>
2024-03-06 13:05:10 +0100

    add green

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 const isBuild = import.meta.env.PROD;
-
+console.log(`Running astro - prefetch enable : ${isBuild}`)
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pgrasland.github.io',
